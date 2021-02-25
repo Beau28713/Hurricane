a560193f76e8c6b402b68d668a12b936bf84a630
Beau Clark <65408911+Beau28713@users.noreply.github.com>
2021-02-25 18:24:13 -0500

    Added Comments

--- Hurricane/Hurricane/Form1.Designer.cs
+++ Hurricane/Hurricane/Form1.Designer.cs
@@ -29,7 +29,6 @@ namespace Hurricane
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.windSpeed = new System.Windows.Forms.TextBox();
             this.hurcCatagory = new System.Windows.Forms.TextBox();
             this.runProg = new System.Windows.Forms.Button();
@@ -40,25 +39,28 @@ namespace Hurricane
             // 
             // windSpeed
             // 
-            this.windSpeed.Location = new System.Drawing.Point(247, 117);
+            this.windSpeed.Location = new System.Drawing.Point(185, 95);
+            this.windSpeed.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.windSpeed.Name = "windSpeed";
-            this.windSpeed.Size = new System.Drawing.Size(193, 22);
+            this.windSpeed.Size = new System.Drawing.Size(146, 20);
             this.windSpeed.TabIndex = 0;
             this.windSpeed.TextChanged += new System.EventHandler(this.windSpeed_TextChanged);
             // 
             // hurcCatagory
             // 
-            this.hurcCatagory.Location = new System.Drawing.Point(513, 117);
+            this.hurcCatagory.Location = new System.Drawing.Point(385, 95);
+            this.hurcCatagory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.hurcCatagory.Name = "hurcCatagory";
-            this.hurcCatagory.Size = new System.Drawing.Size(193, 22);
+            this.hurcCatagory.Size = new System.Drawing.Size(146, 20);
             this.hurcCatagory.TabIndex = 1;
             this.hurcCatagory.TextChanged += new System.EventHandler(this.hurcCatagory_TextChanged);
             // 
             // runProg
             // 
-            this.runProg.Location = new System.Drawing.Point(511, 193);
+            this.runProg.Location = new System.Drawing.Point(194, 157);
+            this.runProg.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.runProg.Name = "runProg";
-            this.runProg.Size = new System.Drawing.Size(97, 37);
+            this.runProg.Size = new System.Drawing.Size(73, 30);
             this.runProg.TabIndex = 2;
             this.runProg.Text = "Run Prog";
             this.runProg.UseVisualStyleBackColor = true;
@@ -66,9 +68,10 @@ namespace Hurricane
             // 
             // closeProg
             // 
-            this.closeProg.Location = new System.Drawing.Point(614, 193);
+            this.closeProg.Location = new System.Drawing.Point(426, 157);
+            this.closeProg.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.closeProg.Name = "closeProg";
-            this.closeProg.Size = new System.Drawing.Size(102, 37);
+            this.closeProg.Size = new System.Drawing.Size(76, 30);
             this.closeProg.TabIndex = 3;
             this.closeProg.Text = "Close Prog";
             this.closeProg.UseVisualStyleBackColor = true;
@@ -77,35 +80,37 @@ namespace Hurricane
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(255, 81);
+            this.label1.Location = new System.Drawing.Point(191, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(166, 17);
+            this.label1.Size = new System.Drawing.Size(127, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "Enter wind speed in MPH";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(538, 84);
+            this.label2.Location = new System.Drawing.Point(404, 68);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(131, 17);
+            this.label2.Size = new System.Drawing.Size(98, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Hurricane Catagory";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(968, 488);
+            this.ClientSize = new System.Drawing.Size(726, 396);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.closeProg);
             this.Controls.Add(this.runProg);
             this.Controls.Add(this.hurcCatagory);
             this.Controls.Add(this.windSpeed);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- Hurricane/Hurricane/Form1.cs
+++ Hurricane/Hurricane/Form1.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
+using System.Windows.Forms;
+/*
+ Take in user wind speed in MPH and output weather it is a class 4,3,2 or 1 Hurricane.
+ Category 5 hurricanes have sustained winds of at least 157 miles per hour. The minimum sustained wind speeds for 
+ categories 4 through 1 are 130, 111, 96, and 74 miles per hour, respectively. Any storm with winds of less than 74
+ miles per hour is not a hurricane.
+ */
 namespace Hurricane
 {
     public partial class Form1 : Form
@@ -19,11 +18,12 @@ namespace Hurricane
 
         private void windSpeed_TextChanged(object sender, EventArgs e)
         {
-
+            //Used for the user input
         }
 
         private void hurcCatagory_TextChanged(object sender, EventArgs e)
         {
+            //Used for outputting the Hurricane Cat. 
             
         }
 
@@ -31,6 +31,7 @@ namespace Hurricane
         {
             var speed = Convert.ToDouble(windSpeed.Text);
 
+            //Hurricane.Text sends data to output text box
             if (speed < 74)
             {
                 hurcCatagory.Text = "Not a Hurc";
@@ -63,7 +64,7 @@ namespace Hurricane
 
         private void closeProg_Click(object sender, EventArgs e)
         {
-            Close();
+            Close();//Button used to close program. If user does not want to use (x). 
         }
     }
 }
